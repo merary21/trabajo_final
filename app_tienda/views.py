62a75b494bad4080a6b05abb1690e8688145648b
merary21 <julissa21araujo@gmail.com>
2022-11-07 01:40:35 -0600

    pagia de quienes somos

--- app_tienda/views.py
+++ app_tienda/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(resquest):
     return render (resquest, 'inicio.html')
+
+def quienessomos(resquest):
+    return render (resquest, 'quienessomos.html')
--- proyectoTECNO/proyectoTECNO/urls.py
+++ proyectoTECNO/proyectoTECNO/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app_tienda.views import inicio 
+from app_tienda.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio)
+    path('', inicio), 
+    path('quienessomos.html', quienessomos)
+    
+    
+    
+   
 ]
